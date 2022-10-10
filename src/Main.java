d1c3efc3e759b4263191ffbff74ffa9c8447a2e7
muhamed-droid <mmasnopita1@etf.unsa.ba>
2022-10-10 13:29:42 +0200

    With username

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        System.out.println("Please enter username: ");
+        Scanner ulaz = new Scanner(System.in);
+        String username = ulaz.nextLine();
+        System.out.println("Hello " + username);
     }
 }
