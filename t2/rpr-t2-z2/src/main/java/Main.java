c5be900a2c71a84c1a08d70165c055450c1b9262
Muhamed Masnopita <63816826+muhamed-droid@users.noreply.github.com>
2022-10-27 17:22:25 +0200

    Delete t2 directory

--- t2/rpr-t2-z2/src/main/java/Main.java
+++
@@ -1,6 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        Osoba enes = new Osoba("Enes", "Hadziedhme");
-        System.out.println(enes);
-    }
-}
--- t2/rpr-t2-z2/src/main/java/Uposlenik.java
+++
@@ -1,6 +0,0 @@
-public class Uposlenik extends Osoba{
-
-    public Uposlenik(String ime, String prezime) {
-        super(ime, prezime);
-    }
-}
